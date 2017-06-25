63af07af4c8314f276ceebedee4d031802ed2073
David Brunnthaler <david@screencode.at>
2017-06-25 21:01:01 +0200

    cipher 21: hex2ascii + vigenère 'trw'

---
+++ 21.php
@@ -0,0 +1,48 @@
+<pre><?php
+    /*
+      hex to ascii
+      --> vigenère cipher
+      --> ignore the "'"
+      --> use keyword 'trw'
+      https://en.wikipedia.org/wiki/Vigen%C3%A8re_cipher
+     */
+
+    $orig = '77726e6b7668656a77676b6b276c6d6b6274616672656567776c6a7368697a70726f6d79656c';
+    echo 'orig:  '.$orig.'<br />';
+
+
+    $step1 = hex2bin($orig);
+    echo 'step1: '.$step1.'<br />';
+
+
+    $step2 = array();
+    for($i=0; $i<strlen($step1); $i++) {
+        $step2[] = $step1[$i];
+    }
+
+
+    $alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    $keyword = 'trw';
+
+    $step3 = array();
+
+    $counter = 0;
+    for($i=0; $i<count($step2); $i++) {
+        if(strpos($alphabet, $step2[$i]) !== false) {
+            $targetPos = strpos($alphabet, $step2[$i]) - strpos($alphabet, $keyword[($counter%strlen($keyword))]);
+
+            if($targetPos>=strlen($alphabet)) {
+                $targetPos = $targetPos - strlen($alphabet);
+            } elseif($targetPos<0) {
+                $targetPos = $targetPos + strlen($alphabet);
+            }
+            $step3[] = $alphabet[$targetPos];
+            $counter ++;
+        } else {
+            $step3[] = $step2[$i];
+        }
+    }
+
+    echo 'step3: '.implode('', $step3);
+
+    ?></pre>
